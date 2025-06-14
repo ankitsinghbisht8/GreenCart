0e4fd60b54b1a8b37c48d41e47bcae696178252b
ankitsinghbisht8 <asbankit8@gmail.com>
2025-06-14 10:58:30 +0530

    initial changes setup and some context

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,26 @@
+//this file is for global state management for variables and functions
+//This code sets up a global state management system in React using the Context API. It allows different components in your React app to share variables and functions without having to pass them manually as props.
+// createContext()--> Creates a context object
+// AppContextProvider--> Provides the shared data/functions to children
+// useAppContext()--> Custom hook to access context easily
+// value--> The object containing variables and functions you want globally accessible
+
+import { createContext, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+
+export const AppContext = createContext();
+
+export const AppContextProvider = ({children})=>{
+
+    const navigate= useNavigate(); // now i can access this components anywhere
+    const [user,setUser]= useState(null);
+    const [isSeller,setIsSeller]= useState(false);
+    const value = {navigate,user,isSeller,setUser,setIsSeller};
+    return <AppContext.Provider value={value}>
+        {children}
+    </AppContext.Provider>
+}
+
+export const useAppContext = ()=>{
+    return useContext(AppContext)
+}
