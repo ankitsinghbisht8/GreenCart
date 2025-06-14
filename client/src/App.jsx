9474f473c38d9e3f089a48470c3e27409bad0952
ankitsinghbisht8 <asbankit8@gmail.com>
2025-06-14 22:18:25 +0530

    banner section done as of now

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { use } from 'react'
 import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import { Route, Routes, useLocation } from 'react-router-dom'
 
 
 const App = () => {
+
+  const isSellerPath= useLocation().pathname.includes('/seller'); //this will check if the current path includes '/seller'
   return (
     <div>
-      <Navbar />
+      {/* This conditionally renders the Navbar component only if the current path does not include '/seller'. */}
+      {isSellerPath ? null : <Navbar />} 
+      <div className={`${isSellerPath ?"":"px-6 md:px-16 lg:px-24 xl:px-32 2xl:px-40"}`}> 
+        {/* This conditionally applies padding based on the current path. If the path includes '/seller', no padding is applied, otherwise, it applies the specified padding. */}
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+        </Routes>
+      </div>
+      
     </div>
   )
 }
