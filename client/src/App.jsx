f9bef28ee67408e1da5d2d78440dd5c21f2907dc
ankitsinghbisht8 <asbankit8@gmail.com>
2025-06-15 13:15:08 +0530

    bestseller and categories added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React, { use } from 'react'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import { Route, Routes, useLocation } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
 
 
 const App = () => {
@@ -10,7 +11,8 @@ const App = () => {
   return (
     <div>
       {/* This conditionally renders the Navbar component only if the current path does not include '/seller'. */}
-      {isSellerPath ? null : <Navbar />} 
+      {isSellerPath ? null : <Navbar />}
+      <Toaster />
       <div className={`${isSellerPath ?"":"px-6 md:px-16 lg:px-24 xl:px-32 2xl:px-40"}`}> 
         {/* This conditionally applies padding based on the current path. If the path includes '/seller', no padding is applied, otherwise, it applies the specified padding. */}
         <Routes>
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -5,18 +5,67 @@
 // useAppContext()--> Custom hook to access context easily
 // value--> The object containing variables and functions you want globally accessible
 
-import { createContext, useContext , useState } from "react";
+import { createContext, useContext , useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { dummyProducts } from "../assets/assets";
+import toast from "react-hot-toast";
 
 export const AppContext = createContext();
 
 export const AppContextProvider = ({children})=>{
 
+    const currency=import.meta.env.VITE_CURRENCY; // this is how we can access the environment variables in react
+
     const navigate = useNavigate(); // now i can access this components anywhere
     const [user,setUser]= useState(null);
     const [isSeller,setIsSeller]= useState(false);
     const [showUserLogin,setShowUserLogin]= useState(false);
-    const value = {navigate,user,isSeller,setUser,setIsSeller,showUserLogin,setShowUserLogin};
+    const [products,setProducts]= useState([]);
+    const [cartItems,setCartItems]= useState({});
+
+    //Fetch products from dummy data or API
+    const fetchProducts = async()=>{
+        setProducts(dummyProducts);
+    }
+    //Add product to cart 
+    const addToCart= (itemId)=>{
+        let cartData= structuredClone(cartItems);
+        if(cartData[itemId]){
+            cartData[itemId] += 1; // Increment quantity if item already exists
+        }
+        else{
+            cartData[itemId] = 1; // Add new item with quantity 1
+        }
+        setCartItems(cartData);
+        toast.success("Item added to cart successfully!");
+    }
+
+    //Update cart item quantity
+    const updateCartItem = (itemId , quantity) => {
+        let cartData =structuredClone(cartItems);
+        cartItems[itemId] = quantity;
+        setCartItems(cartData);
+        toast.success("Cart updated successfully!");
+    }
+
+    //remove item from cart
+    const removeCartItem=(itemId)=>{
+        let cartData= structuredClone(cartItems);
+        if(cartData[itemId]){
+            cartData[itemId] -= 1; // Remove item from cart
+            if(cartData[itemId] <= 0) {
+                delete cartData[itemId]; // If quantity is 0, remove item from cart
+            }
+        }
+        toast.success("Removed from cart!");
+        setCartItems(cartData);
+    }
+    
+    useEffect(()=>{
+        fetchProducts();
+    },[])
+
+    const value = {navigate,user,isSeller,setUser,setIsSeller,showUserLogin,setShowUserLogin,products,currency,addToCart,updateCartItem,removeCartItem,cartItems};
     return <AppContext.Provider value={value}>
         {children}
     </AppContext.Provider>
